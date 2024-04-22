d453edf8083584951e247ee24562c9626ab228a2
niklm <nm13@live.missouristate.edu>
2024-04-21 22:43:49 -0500

    ch 12 final commit

---
+++ app/src/main/java/com/msu/morrison/criminalintent/database/CrimeDao.kt
@@ -0,0 +1,19 @@
+package com.msu.morrison.criminalintent.database
+
+import androidx.room.Dao
+import androidx.room.Query
+import com.msu.morrison.criminalintent.Crime
+import kotlinx.coroutines.flow.Flow
+import java.util.*
+
+
+@Dao
+interface CrimeDao {
+    @Query("SELECT * FROM crime")
+    fun getCrimes(): Flow<List<Crime>>
+
+    @Query("SELECT * FROM crime WHERE id=(:id)")
+    fun getCrime(id: UUID): Crime
+
+
+}
